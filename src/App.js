2e8a94bdb01f7eec2564648652ecf247e88dcd9e
Juan Lucero <jlucero@alhpaworks.tech>
2021-10-13 15:57:25 -0700

    Added global state object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [hasGoldKey, setHasGoldKey] = useState(false);
+  const [hasSilverKey, setHasSilverKey] = useState(false);
+  const [rooms, setRooms] = useState([
+    {
+      name: "Living Room",
+      description: "living room description goes here",
+      image: logo,
+    },
+    {
+      name: "Kitchen",
+      description: "kitchen description goes here",
+      image: logo,
+    },
+  ]);
+  const [events, setEvents] = useState([
+    {
+      name: "Ghost",
+      description: "ghost description goes here",
+      image: logo,
+      actions: [
+        { pass: 'pass goes here' },
+        { fail: 'fail goes here, goes into fail screen' }, 
+        { redo: 'try again goes here' },
+      ]
+    },
+    {
+      name: "Warewolf",
+      description: "warewolf description goes here",
+      image: logo,
+      actions: [
+        { pass: 'pass goes here' },
+        { fail: 'fail goes here, goes into fail screen' }, 
+        { redo: 'try again goes here' },
+      ]
+    },
+  ]);
+ 
   return (
     <div className="App">
       <header className="App-header">
