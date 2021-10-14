e70b9a235e44ed0f240501137d47990272ddc383
Garet Hough <garetshough@gmail.com>
2021-10-14 15:44:09 -0700

    Finished adding self generated buttons with map function

--- src/components/Hallway.js
+++ src/components/Hallway.js
@@ -2,14 +2,15 @@ import React from 'react'
 import img from '../img/Hall-option-1.jpg'
 import '../main.css';
 
-const Hallway = () => {
+const Hallway = (props) => {
     return (
         <div>
-
             <h1>Hallway</h1>
             <img className='hallway' src={img} alt="" />
             <p>description of the hallway</p>
-            <button></button>
+            {props.rooms.map((room, key)=> (
+<a href={`/room/${room.name}`}><button>{room.name}</button></a>
+            ))}
         </div>
     )
 };
