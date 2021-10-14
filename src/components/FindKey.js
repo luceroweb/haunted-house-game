fdc5ea2f3ee360df60a048c46bfb4bc920c9cdc4
Tesfa-Worku <hopegorg@gmail.com>
2021-10-14 18:36:18 -0400

    first

---
+++ src/components/FindKey.js
@@ -0,0 +1,46 @@
+// import React, { useState } from 'react';
+
+// function FindKey(){
+
+//     //const[hasHouseKey, setHasHouseKey] = useState('Hi there');
+    
+//     // if(props.events.actions[0]){
+//     //     setHasHouseKey("You got the Golden key ")
+//     // }
+//     //     console.log(hasHouseKey)
+//     return (
+//         <div>
+
+
+//             <img src={} alt=""/>
+
+
+//             <p>
+//                 {/* {hasHouseKey.props} */}
+//             </p>
+
+
+//             <p>
+//             Description: “The person who built it sold it. The person who bought it never used it. The person who used it never saw it...what is it?”
+//             </p>
+
+//             <p>
+//             (Answer is a coffin)
+//             </p>
+
+//             <p>
+//             A coffin = “You are wise enough to escape. Wisdom is the key to survival” An illuminated silver key appears before the team and the door they entered through opens.
+//             </p>
+
+//             <p>
+//             A meal = “You have failed and will meet your doom!” = try again
+//             A table = “You have failed and will meet your doom!” = try again
+//             </p>
+
+
+//         </div>
+//     );
+// };
+
+// export default FindKey;
+
