6571647a02b45b0a8669641cadbb10a204862cb5
Xavier Mercado <xmercado1576@gmail.com>
2021-10-13 18:00:28 -0700

    presentKey function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   const [hasGoldKey, setHasGoldKey] = useState(false);
   const [hasSilverKey, setHasSilverKey] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
   const [rooms, setRooms] = useState([
     {
       name: "Living Room",
@@ -17,6 +18,7 @@ function App() {
       image: logo,
     },
   ]);
+
   const [events, setEvents] = useState([
     {
       name: "Ghost",
@@ -39,7 +41,31 @@ function App() {
       ]
     },
   ]);
-
+  
+  // shows gameOver success or fail screen when player gets key
+  const presentKey = () => {
+    const winMessage = '';
+    // fake key and keep playing
+    if (hasGoldKey) {
+      winMessage='The group makes their way to the front door.  Juan takes the key out of his pocket and inserts it into the keyhole, but just as the tip of the key touches the lock, it smashes into the lock, the gold wrapping peels away and reveals semi-melted chocolate.';
+      setHasGoldKey(false);
+    }
+    // real key and win
+    else if (hasSilverKey) {
+      winMessage=`"You return to the group with the key.  The cage containing Juan and the rest of the team lifts.  The team rushes out of the cage, and passes you to the door.  As you hold out the key, the group parts and you spot the door.  You insert the key into the keyhole and turn.  As the door unlocks, you push the heavy door open.  Fresh air rushes past you and into the house.  You and the team spill out into the street as you spot a figure strolling up the street to the house.
+      
+      “Irma?! What are you doing here?!” Eyob questions.
+      
+      “Hi everyone.  You’re early for the Bitwise Haunted House!” Irma says, a smile plastered on her face as she points to the small “Bitwise Industries” sign just above the doorway."`;
+      setHasSilverKey(false);
+      setGameOver(true);
+      // when gameOver is true, show appropriate ending using if condition and function or ternary conditional rendering
+    }
+    // keep playing
+    else {
+      return;
+    }
+  };
 
   return (
     <div className="App">
