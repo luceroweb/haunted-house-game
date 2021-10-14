0a3399be19260a9c5e8702cbb85ec0dc0db64ee5
Eyobel Legese <eyobel7k@gmail.com>
2021-10-14 15:16:49 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import logo from "./logo.png";
+import "./App.css";
+import GameOver from "./components/GameOver";
 function App() {
   const [hasGoldKey, setHasGoldKey] = useState(false);
   const [hasSilverKey, setHasSilverKey] = useState(false);
+  const [isGameOver, setisGameOver] = useState(true);
   const [rooms, setRooms] = useState([
     {
       name: "Living Room",
@@ -23,42 +24,31 @@ function App() {
       description: "ghost description goes here",
       image: logo,
       actions: [
-        { pass: 'pass goes here' },
-        { fail: 'fail goes here, goes into fail screen' }, 
-        { redo: 'try again goes here' },
-      ]
+        { pass: "pass goes here" },
+        { fail: "fail goes here, goes into fail screen" },
+        { redo: "try again goes here" },
+      ],
     },
     {
       name: "Warewolf",
       description: "warewolf description goes here",
       image: logo,
       actions: [
-        { pass: 'pass goes here' },
-        { fail: 'fail goes here, goes into fail screen' }, 
-        { redo: 'try again goes here' },
-      ]
+        { pass: "pass goes here" },
+        { fail: "fail goes here, goes into fail screen" },
+        { redo: "try again goes here" },
+      ],
     },
   ]);
-
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <GameOver message='game Over' isGameOver={isGameOver} />
+        
       </header>
     </div>
   );
 }
-
 export default App;
