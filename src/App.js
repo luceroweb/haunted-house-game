a696462cf17235ec1c763965dc0e199168d7bc5a
Grace Aranico <grace.aranico@gmail.com>
2021-10-14 14:09:08 -0700

    created json files for events and room arrays, uploaded images

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RoomData from './components/RoomData.json';
+import EventsData from './components/EventsData.json';
+
 
 function App() {
   const [hasGoldKey, setHasGoldKey] = useState(false);
   const [hasSilverKey, setHasSilverKey] = useState(false);
-  const [rooms, setRooms] = useState([
-    {
-      name: "Living Room",
-      description: "living room description goes here",
-      image: logo,
-    },
-    {
-      name: "Kitchen",
-      description: "kitchen description goes here",
-      image: logo,
-    },
-  ]);
-  const [events, setEvents] = useState([
-    {
-      name: "Ghost",
-      description: "ghost description goes here",
-      image: logo,
-      actions: [
-        { pass: 'pass goes here' },
-        { fail: 'fail goes here, goes into fail screen' }, 
-        { redo: 'try again goes here' },
-      ]
-    },
-    {
-      name: "Warewolf",
-      description: "warewolf description goes here",
-      image: logo,
-      actions: [
-        { pass: 'pass goes here' },
-        { fail: 'fail goes here, goes into fail screen' }, 
-        { redo: 'try again goes here' },
-      ]
-    },
-  ]);
+  const [rooms, setRooms] = useState(RoomData.rooms);
 
+  const [events, setEvents] = useState(EventsData.json);
 
   return (
     <div className="App">
