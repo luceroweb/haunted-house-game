b79dd8e9f49a34388c8ebcdf19e619db60249fcc
Grace Aranico <grace.aranico@gmail.com>
2021-10-15 15:49:09 -0700

    fixed spacing

--- src/components/EventsData.js
+++ src/components/EventsData.js
@@ -203,9 +203,9 @@ const eventData = [
         response: "“I was mistaken, try again.”",
         },
         { 
-          type: "redo",
-          action: "Believe the weird little alien, take the key.", 
-          response: "The group makes their way to the front door.  Juan takes the key out of his pocket and inserts it into the keyhole, but just as the tip of the key touches the lock, it smashes into the lock, the gold wrapping peels away and reveals semi-melted chocolate.",
+        type: "redo",
+        action: "Believe the weird little alien, take the key.", 
+        response: "The group makes their way to the front door.  Juan takes the key out of his pocket and inserts it into the keyhole, but just as the tip of the key touches the lock, it smashes into the lock, the gold wrapping peels away and reveals semi-melted chocolate.",
         },   
       ]
     }
