567d5d95ecd4a6c735f2ded616f9084567799a2b
Myles <mazehko@alphaworks.tech>
2021-10-15 11:59:04 -0700

    Created Header.js component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import house from '../img/rooms/house.jpeg';
 
-export default function Header(/*should pull from some sort of state here*/) {
-    return (
-        <div>
-            <h1>HAUNTED HOUSE GAME</h1>HAUNTED HOUSE GAME 
-        </header>
-        )
-    };
-
-
+export default function StartGame({hallway}) {
+  return (
+    <div>
+      <img src={house} alt="" />
+      <div class="header-div">
+        <h1 class="main-style-header">THE HAUNTED HOUSE GAME</h1>
+        <h3>By the React Apprentice Team.</h3>
+        <h2>&#128123; &#x1F47B;  &#128123; &#x1F47B; </h2>
+      </div>
+      
+      <p>The Bitwise React Apprentices were out trick or treating on a dark and windy Halloween eve, when they stumbled across a creepy, rundown home at the end of a half-built construction site.  Juan, the leader of the group, walked up to the door, rang the doorbell and no one answered.</p>
+      <p>He jiggled the door and it swung open.</p>
+      <p>“Is there anyone home?” asks Myles.  “Do you think they have Skittles?”</p>
+      <p>“Does it look like they have Skittles? They barely have time to clean from the looks of it!” Grace says, eyeing the cobwebs in the corner of the doorway.</p>
+      <p>“Ooohh… I think I see some Skittles in the hallway!” Darla exclaims, pointing deeper into the darkened space just beyond the door.</p>
+      <a href="/hallway"><button>Continue...</button></a>
+    </div>
+  )
+};
