9144374e4e6b195dc5530f8bbcf8ccd9e0b5e588
Darla <dbrown@alphaworks.tech>
2021-10-15 11:04:12 -0700

    update

--- src/App.js
+++ src/App.js
@@ -12,19 +12,21 @@ import EventsData from './components/EventsData.json';
 import StartGame from './components/StartGame';
 import Event  from './components/Event';
 import GameOver from './components/GameOver';
+import Random from './util/Random';
 
 function App() {
   const [hasGoldKey, setHasGoldKey] = useState(false);
   const [hasSilverKey, setHasSilverKey] = useState(false);
   const [rooms, setRooms] = useState(RoomData.rooms);
-  const [events, setEvents] = useState(EventsData.json);
+  const [events, setEvents] = useState(EventsData.events);
 
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
           {/* <StartGame /> */}
-          <Event actions={Random.selectEvent(events)} />
+          
+          <Event event={Random.selectEvent(events)} />
         </Route>
         <Route exact path="/hallway">
           <div>Hallway</div>
