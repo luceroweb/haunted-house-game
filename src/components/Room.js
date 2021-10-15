63fbbe5961f15dbd89709431414b1e1f53826a12
Jason Smith <jasons@alphaworks.tech>
2021-10-15 11:31:20 -0700

    finallized the reusable asset css

--- src/components/Room.js
+++ src/components/Room.js
@@ -10,9 +10,7 @@ function Room(props) {
   return (
     <div>
       <h1>{found[0].name}</h1>
-      <div>
-        <img src={found[0].image} alt="" />
-      </div>
+      <img src={found[0].image} alt="" />
 
       <p>{found[0].description}</p>
       <a href="/hallway">
