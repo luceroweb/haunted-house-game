9a857090e1c42bae6e4bda6b95c5231250dd7a10
Myles <mazehko@alphaworks.tech>
2021-10-14 18:05:46 -0700

    Flavicon changed to haunted house.

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Header(/*should pull from some sort of state here*/) {
+    return (
+        <header>HAUNTED HOUSE GAME</header>
+    )
+    };
