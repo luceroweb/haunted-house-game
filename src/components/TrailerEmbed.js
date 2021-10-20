b4f86aad64cbad0477abef76a608eb23fb17d265
Jason Smith <jasons@alphaworks.tech>
2021-10-20 09:04:49 -0700

    most of the styling is complete

--- src/components/TrailerEmbed.js
+++ src/components/TrailerEmbed.js
@@ -4,15 +4,19 @@ import ReactPlayer from 'react-player'
 export const TrailerEmbed = () => {
     console.log(ReactPlayer)
     return (
-        <div>
+        <div id="trailer-embed">
             <h3> Haunted House Project</h3>
-                <ReactPlayer
-                    url='https://www.youtube.com/watch?v=5KCUT1os_ug'
-                    playing={true}
-                    muted={true}
-                    controls={true}
-                />
+            <ReactPlayer
+                className="player"
+                width='80%'
+                url='https://www.youtube.com/watch?v=5KCUT1os_ug'
+                playing={true}
+                muted={true}
+                controls={true}
+            />
+            <div className="btn-wrap">
                 <a href="/startgame"><button>Are You Ready?</button></a>
+            </div>
         </div>
     )
 }
