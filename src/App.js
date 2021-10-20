7e5d4feedef08fd24ee4312a1add42ac35c59e63
alberm <albertmartinez16@gmail.com>
2021-10-20 15:07:01 -0700

    add Audio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import RoomData from "./components/RoomData.js";
@@ -6,16 +6,19 @@ import EventsData from "./components/EventsData";
 import StartGame from "./components/StartGame";
 import Hallway from "./components/Hallway";
 import Room from "./components/Room";
+import Audio from "./components/Audio";
 
 function App() {
 	const rooms = RoomData;
 	const events = EventsData;
+  const [ audioControls, setAudioControls ] = useState([]);
 
   return (
     <BrowserRouter>
+      <Audio onLoaded={(controls) => setAudioControls(controls)} />
       <Switch>
         <Route exact path="/">
-          <StartGame />
+          <StartGame audioControls={audioControls}/>
         </Route>
         <Route exact path="/hallway">
           <Hallway rooms={rooms} />
