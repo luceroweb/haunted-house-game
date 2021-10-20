2bf5fd63b11cc0f9caf6ee48b692ac0a0abd43de
srosenberg74 <srosenberg@alphaworks.tech>
2021-10-20 15:03:39 -0700

    continue button not working

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,48 +1,84 @@
 import React, { useState } from "react";
 import ResultAction from "./ResultAction";
 import GameOver from "./GameOver";
+import "./Modal.css";
 
+const Event = ({
+  event,
+  isGameOver,
+  setIsGameOver,
+  hasSilverKey,
+  hasGoldKey,
+  setHasGoldKey,
+  setHasSilverKey,
+  beginEvent,
+  setBeginEvent,
+}) => {
+  const [action, setAction] = useState({});
+  const [selectedAction, setSelectedAction] = useState(-1);
+  const [showDialog, setShowDialog] = useState(false);
 
-const Event = ({ event, isGameOver, setIsGameOver, hasSilverKey, hasGoldKey, setHasGoldKey, setHasSilverKey }) => {
-	const [action, setAction] = useState({});
-	const [selectedAction, setSelectedAction] = useState(-1);
+  const displayEvent = (time) => {
+    setTimeout(() => {
+      setShowDialog(true);
+    }, time);
+  };
 
-	return (
-		<div>
-			<h3>{event.name}</h3>
-			<img src={event.image} alt="" />
-			<p>{event.description}</p>
-			{event.actions.map((currentAction, i) => (
-				<div key={i}>
-          {(!isGameOver) &&
-            <button
-              onClick={() => {
-                setAction(currentAction);
-                setSelectedAction(i);
-              }}
-            >
-              {currentAction.action}
-            </button>
-          }
-					<ResultAction 
-						i={i} 
-						action={action} 
-						selectedAction={selectedAction} 
-						setHasSilverKey={setHasSilverKey}
-						setHasGoldKey={setHasGoldKey}
-            setIsGameOver={setIsGameOver}
-            hasGoldKey={hasGoldKey}
-            hasSilverKey={hasSilverKey}
-					/>
-          <GameOver
-						i={i} 
-            isGameOver={isGameOver}
-						selectedAction={selectedAction} 
-          />
-				</div>
-			))}
-		</div>
-	);
+  const closeEvent = (time) => {
+    setTimeout(() => {
+      setShowDialog(false);
+    }, time);
+  };
+
+  if (beginEvent) {
+    displayEvent(5000);
+    setBeginEvent(false);
+  }
+
+  return (
+    <>
+      {showDialog && (
+        <div className="modal">
+          <h3>{event.name}</h3>
+          <img src={event.image} alt="" />
+          <p>{event.description}</p>
+          {event.actions.map((currentAction, i) => (
+            <div key={i}>
+              {!isGameOver && (
+                <button
+                  onClick={() => {
+                    setAction(currentAction);
+                    setSelectedAction(i);
+                  }}
+                >
+                  {currentAction.action}
+                </button>
+              )}
+              <ResultAction
+                i={i}
+                action={action}
+                selectedAction={selectedAction}
+                setHasSilverKey={setHasSilverKey}
+                setHasGoldKey={setHasGoldKey}
+                setIsGameOver={setIsGameOver}
+                hasGoldKey={hasGoldKey}
+                hasSilverKey={hasSilverKey}
+                closeEvent={closeEvent}
+                name={event.name}
+                setShowDialog={setShowDialog}
+              />
+              <GameOver
+                i={i}
+                isGameOver={isGameOver}
+                selectedAction={selectedAction}
+                message={action.response}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Event;
--- src/components/Room.js
+++ src/components/Room.js
@@ -1,41 +1,64 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Event from './Event';
-import Random from '../util/Random';
+import Event from "./Event";
+import Random from "../util/Random";
+// import EventModal from "./EventModal";
+// import "./Modal.css";
 
 function Room(props) {
   const { name } = useParams();
-  const [isGameOver, setIsGameOver] = useState(false);	const [hasGoldKey, setHasGoldKey] = useState(false);
-	const [hasSilverKey, setHasSilverKey] = useState(false);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [hasGoldKey, setHasGoldKey] = useState(false);
+  const [hasSilverKey, setHasSilverKey] = useState(false);
+  const [beginEvent, setBeginEvent] = useState(true);
+  // const [showDialog, setShowDialog] = useState(false);
 
   const found = props.rooms.filter(
     (room) => room.name.toLowerCase() === name.toLowerCase()
   );
 
+  // const renderNow = () => {
+  //   setTimeout(() => {
+  //     setShowDialog(true);
+  //   }, 5000);
+  // };
+
+  // renderNow();
+  // useEffect(() => {
+  //   setTimeout(renderNow, 5000);
+  // }, []);
+  const handleBeginEvent = () => {
+    setBeginEvent(true);
+  }
+
   return (
     <div>
+      {/* {showDialog && ( */}
+        <Event
+          event={Random.selectEvent(props.events)}
+          isGameOver={isGameOver}
+          setIsGameOver={setIsGameOver}
+          hasGoldKey={hasGoldKey}
+          setHasGoldKey={setHasGoldKey}
+          hasSilverKey={hasSilverKey}
+          setHasSilverKey={setHasSilverKey}
+          setBeginEvent={setBeginEvent}
+          beginEvent={beginEvent}
+        />
+      {/* )} */}
+
       <h1>{found[0].name}</h1>
       <img src={found[0].image} alt="" />
 
       <p>{found[0].description}</p>
+      {/* {() => countDownTillRender} */}
+      <button>Search Room</button>
 
-      <Event
-        event={Random.selectEvent(props.events)}
-        isGameOver={isGameOver}
-        setIsGameOver={setIsGameOver}
-        hasGoldKey={hasGoldKey}
-        setHasGoldKey={setHasGoldKey}
-        hasSilverKey={hasSilverKey}
-        setHasSilverKey={setHasSilverKey}
-      />
-
-      {(!isGameOver || !hasSilverKey) &&
+      {(!isGameOver || !hasSilverKey) && (
         <a href="/hallway">
-          <button className="backToHomeBtn" >
-            Back to Hallway
-          </button>
+          <button className="backToHomeBtn" onClick={()=>handleBeginEvent}>Back to Hallway</button>
         </a>
-      }
+      )}
     </div>
   );
 }
