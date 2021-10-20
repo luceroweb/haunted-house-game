f29e0428f7c832d046979e7a9d339bfa12dc4786
Jason Smith <jasons@alphaworks.tech>
2021-10-20 10:42:27 -0700

    merged from staging

--- src/components/TrailerEmbed.js
+++ src/components/TrailerEmbed.js
@@ -4,23 +4,14 @@ import ReactPlayer from 'react-player'
 export const TrailerEmbed = () => {
     console.log(ReactPlayer)
     return (
-<<<<<<< HEAD
         <div id="trailer-embed">
-=======
-        <div className='player-wrapper'>
->>>>>>> origin/staging
             <h3> Haunted House Project</h3>
             <div className="player">
                 <ReactPlayer
-<<<<<<< HEAD
                     className='react-player'
                     width='100%'
                     height='100%'
                     url='https://www.youtube.com/watch?v=5KCUT1os_ug'
-=======
-                className='react-player'
-                    url='https://www.youtube.com/watch?v=zsRjgoRZlTY'
->>>>>>> origin/staging
                     playing={true}
                     muted={true}
                     controls={true}
