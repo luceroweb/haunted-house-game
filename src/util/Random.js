d0360b8019894b0e4457247f80876f3265569cdc
alberm <albertmartinez16@gmail.com>
2021-10-21 10:33:16 -0700

    edit comments, add getRandomNumber

--- src/util/Random.js
+++ src/util/Random.js
@@ -1,10 +1,22 @@
 
 export default class Random {
   /**
+   * Get a random number from 0 to below max
+   * @param {number} max 
+   * @returns {number} a random number
+   * call this function with a max number that random numbers should be less than.
+   * ex: Random.getRandomNumber(10) will return a number between 0 to 9, inclusive
+   */
+  static getRandomNumber(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  /**
    * Gets a random event from events state
    * @param {Array} events
    * @returns {Object} a random event
-   * call selectEvent and pass the events state. ex: Random.selectEvent(events)
+   * call selectEvent and pass the events state.
+   * ex: Random.selectEvent([{ name: 'werewolf' }, { name: 'zombie' }]) could return { name: 'zombie' }
    */
   static selectEvent(events) {
     // type checking safety
@@ -12,33 +24,32 @@ export default class Random {
       throw new TypeError(`selectEvent(): Received type ${events === null ? 'null' : typeof events}, expected an Array`);
     }
 
-    const randomNumber = Math.floor(Math.random() * events.length);
+    const randomNumber = Random.getRandomNumber(events.length);
     return events[randomNumber];
   }
 
 /**
- * Gets exclusive random elements from an array
+ * Gets a shuffled subset array from an array
  * @param {Array} list
  * @param {number} amount
  * @return {Array}
  * call this function by passsing an array and the number of random elements you want back.
- * ex: Random.selectRandomElements(myList, 2) will return an array of two random elements from myList
+ * ex: Random.selectRandomElements([ 1,2,3,4 ], 2) could return [ 2,5 ]
  */
   static selectRandomElements(list, amount) {
-  // limit amount to list length 
+  // limit amount to list length if user sends amount greater than list.length
   if (amount > list.length) {
-    amount -= (amount - list.length)
+    amount -= (amount - list.length);
   }
-  const random = max => Math.floor(Math.random() * max);
-
+  
   const maxStartIndex = list.length - amount;
-  const randomStartIndex = random(maxStartIndex + 1); // +1 because of how slice works
+  const randomStartIndex = Random.getRandomNumber(maxStartIndex + 1);
   const endIndex = randomStartIndex + amount;
   const randomList = list.slice(randomStartIndex, endIndex);
 
   // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/2450976#2450976
   for (let i = randomList.length - 1; i >= 0; --i) {
-    const randomIndex = random(i);
+    const randomIndex = Random.getRandomNumber(i);
     // [ randomList[i], randomList[randomIndex] ] = [ randomList[randomIndex], randomList[i] ];
     const tmp = randomList[i];
     randomList[i] = randomList[randomIndex];
