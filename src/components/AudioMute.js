59cecdc794572e2be3a306031022af9ee90101c4
alberm <albertmartinez16@gmail.com>
2021-10-21 10:35:16 -0700

    whitespace edits, no changes to code

--- src/components/AudioMute.js
+++ src/components/AudioMute.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { GiSpeaker, GiSpeakerOff } from 'react-icons/gi';
 
-export default function AudioMute ({audioOn, setAudioOn}) {
-    return (
-        <div>
-            <button onClick={() => setAudioOn(!audioOn)}>
-                {
-                    audioOn ?
-                    <GiSpeaker /> :
-                    <GiSpeakerOff />
-                }
-                </button>
-        </div>
-    );
+export default function AudioMute({ audioOn, setAudioOn }) {
+  return (
+    <div>
+      <button onClick={() => setAudioOn(!audioOn)}>
+        {
+          audioOn ?
+            <GiSpeaker /> :
+            <GiSpeakerOff />
+        }
+      </button>
+    </div>
+  );
 };
