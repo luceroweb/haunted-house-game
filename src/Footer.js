c073d5d28a7ff846731b65a85675bf1b41f05eb4
Jason Smith <jasons@alphaworks.tech>
2021-10-21 17:22:24 -0700

    commit before merge

--- src/Footer.js
+++ src/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div class="footer-div">
+        <div className="footer-div">
             <footer>
-            <h1 class="footer-style">THE HAUNTED HOUSE GAME</h1>
+            <h1 className="footer-style">THE HAUNTED HOUSE GAME</h1>
             <h3>By the Bitwise Industries React Apprentice Team.</h3>
             <h2>&#128123; &#x1F47B;  &#128123; &#x1F47B; </h2>
             </footer>   
