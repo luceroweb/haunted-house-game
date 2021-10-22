50f6d8df2b67214596d0cd8b56902cd998aa941e
alberm <albertmartinez16@gmail.com>
2021-10-22 03:06:07 -0700

    add audio

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -9,26 +9,33 @@ function EventModal(props) {
   const [informedOfSilverKey, setInformedOfSilverKey] = useState(true);
   const [informedOfGoldKey, setInformedOfGoldKey] = useState(true);
 
+  // event sounds
   const noiseStarter = {
     Ghost: { sound: ghostScream, volume: 0.01 },
-    Werewolf: { sound: werewolf, volume: 0.05 },
-    Zombie: { sound: zombieMoan, volume: 0.05},
-    "Chainsaw Murderer": { sound: chainsaw, volume: 0.05 },
-    "The Talking Heads": { sound: evilLaugh, volume: 0.05 },
+    Werewolf: { sound: werewolf, volume: 0.10 },
+    Zombie: { sound: zombieMoan, volume: 0.10 },
+    "Chainsaw Murderer": { sound: chainsaw, volume: 0.10 },
+    "The Talking Heads": { sound: evilLaugh, volume: 0.10 },
 
   }
   const soundName = props.event.name;
-  const [playSound, soundData] = useSound(noiseStarter[soundName]?.sound, {
-    soundEnabled: props.audioOn, volume: noiseStarter[soundName]?.volume
-  });
+  const [playSound, soundData] = useSound(
+    noiseStarter[soundName]?.sound, 
+    { soundEnabled: !props.hasChosenAction && props.audioOn,
+    volume: noiseStarter[soundName]?.volume,
+    interrupt: true }
+  );
   
-  if (!props.audioOn) {
+  if (props.hasChosenAction || !props.audioOn) {
     soundData.stop();
+  } else {
+    // setTimeout hack to place playSound() in back of event queue 
+    setTimeout(() => playSound(), 0);
   }
   // For if they just received either key
   if (!informedOfSilverKey || !informedOfGoldKey) {
     return (
-      <div className="event-modal" id="event" onMouseOverCapture={()=>playSound()}>
+      <div className="event-modal" id="event" >
         <p>{props.action.response}</p>
         <KeyDisplay
           hasGoldKey={props.hasGoldKey}
@@ -53,7 +60,7 @@ function EventModal(props) {
     //For when the event first renders and they haven't chosen an action yet
     <>
       {!props.hasChosenAction && !props.isGameOver ? (
-        <div className="event-modal" id="event" onMouseOverCapture={()=>playSound()}>
+        <div className="event-modal" id="event" >
           <div style={{ margin: "0" }}>
             <h3>{props.event.name}</h3>
             <img src={props.event.image} alt="" />
@@ -101,7 +108,7 @@ function EventModal(props) {
         !props.isGameOver &&
         informedOfSilverKey &&
         informedOfGoldKey && (
-          <div className="event-modal" id="event" onMouseOverCapture={()=>playSound()}>
+          <div className="event-modal" id="event" >
             <div style={{ margin: "0" }}>
               <h3>{props.event.name}</h3>
               <img src={props.event.image} alt="" />
