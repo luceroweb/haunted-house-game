f2a6e7940950ca35e9dad51563c2fd85a08d0f26
Jason Smith <jasons@alphaworks.tech>
2021-10-28 16:38:29 -0700

    turned off forced display of key used for testing

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -9,7 +9,7 @@ function Inventory(props) {
 				setAudio={props.setAudio}>
 			</AudioMute>
             { props.goldKey && <span id="gold-key" title="Gold Key"><i class="fas fa-key"></i></span> }
-            { !props.silverKey && <span id="silver-key" title="Silver Key"><i class="fas fa-key"></i></span> }            
+            { props.silverKey && <span id="silver-key" title="Silver Key"><i class="fas fa-key"></i></span> }            
 		</div>
 	);
 }
