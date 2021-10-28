da957de3175bb395d065729a40182c2baf10ac05
Juan Lucero <jlucero@alhpaworks.tech>
2021-10-28 15:09:59 -0700

    Converted GameWon component to use storyData array

--- src/components/GameWon.js
+++ src/components/GameWon.js
@@ -6,6 +6,7 @@ import image from "../img/events/win.jpeg";
 import Header from "./Header";
 import silverKey from "../img/events/silver-key.jpg";
 import Random from "../util/Random";
+import { gamewon } from "./storyData";
 
 function GameWon({ audioOn, onGameOver }) {
   // useState to prevent infinite loop bug when passing random sounds to useSound
@@ -22,14 +23,6 @@ function GameWon({ audioOn, onGameOver }) {
     setTimeout(() => playGameOverWin(), 0);
   }
 
-  const gameWonText = <div>
-    <p>You return to the group with the key. The cage containing Juan and the rest of the team lifts. The team rushes out of the cage, and passes you to the door. As you hold out the key, the group parts and you spot the door. You insert the key into the keyhole and turn. As the door unlocks, you push the heavy door open. Fresh air rushes past you and into the house. You and the team spill out into the street as you spot a figure strolling up the street to the house.</p>
-
-    <p>“Irma?! What are you doing here?!” Eyob questions.</p>
-
-    <p>“Hi everyone. You’re early for the Bitwise Haunted House!” Irma says, a smile plastered on her face as she points to the small “Bitwise Industries” sign just above the doorway.</p>
-  </div>
-
   return (
     <div id="game-won">
       <Header />      
@@ -41,7 +34,13 @@ function GameWon({ audioOn, onGameOver }) {
         <img src={image} alt="" />
       </div>
 
-      {gameWonText}
+      <div>
+        {gamewon.map(
+          (paragraph, index) => (
+            <p key={index}>{paragraph}</p>
+          )
+        )}
+      </div>
 
       <div className="btn-wrap">
         <Link to="/">
